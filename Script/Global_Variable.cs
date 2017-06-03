957ad884083d8497641ee8923d87bc3b9ddc5cab
Jialingkun <djaka.tingkir2@gmail.com>
2017-06-03 23:08:57 +0700

    Random piece template

--- Script/Global_Variable.cs
+++ Script/Global_Variable.cs
@@ -2,16 +2,31 @@
 using System.Collections;
 using UnityEngine.UI;
 public class Global_Variable : MonoBehaviour {
-	public Sprite[] toddlerSprites;
+	public Sprite[] stageImages;
 	private string stageID;
+	private GameObject selectedTemplate;
 	// Use this for initialization
 	void Start () {
 		stageID = PlayerPrefs.GetString ("selectedStage");
 		string level = stageID.Substring(0,2);
 		int stageNum = int.Parse(stageID.Substring(3,2));
-		if (Equals (level, "ts")) {
-			GameObject.Find ("Image").GetComponent<Image> ().sprite = toddlerSprites[stageNum];
+
+		int templateNum = Random.Range (1, 3);
+		string templateName = level + "Template" + templateNum;
+		Debug.Log (templateName);
+
+		selectedTemplate = GameObject.Find (templateName);
+		GameObject.Find ("tsTemplate1").SetActive (false);
+		GameObject.Find ("tsTemplate2").SetActive (false);
+		selectedTemplate.SetActive (true);
+
+
+		foreach (GameObject stageSprite in GameObject.FindGameObjectsWithTag("stageImage")) {
+			stageSprite.GetComponent<Image> ().sprite = stageImages [stageNum];
 		}
+		/*if (Equals (level, "ts")) {
+			GameObject.Find ("Image").GetComponent<Image> ().sprite = ;
+		}*/
 
 	}
 
