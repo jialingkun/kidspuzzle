dc6c89cf95db9b43e24102b775ef6fd03339cbb1
Jialingkun <djaka.tingkir2@gmail.com>
2017-06-05 00:31:29 +0700

    Resize and Drag

--- Script/Global_Variable.cs
+++ Script/Global_Variable.cs
@@ -2,16 +2,31 @@
 using System.Collections;
 using UnityEngine.UI;
 public class Global_Variable : MonoBehaviour {
+	public float pieceSpeed;
+	public float waitingTime;
 	public Sprite[] stageImages;
 	private string stageID;
+	private string level;
 	private GameObject selectedTemplate;
+	private GameObject[] pieceObject;
+	private RectTransform[] piecePos;
+	private RectTransform[] pieceTargetPos;
+
+	private int pieceCount;
+	private int countArrived;
+	private bool[] pieceArrived;
+	private bool waiting;
+	private bool reachTarget;
 	// Use this for initialization
 	void Start () {
+		waiting = true;
+		reachTarget = false;
+		countArrived = 0;
 		stageID = PlayerPrefs.GetString ("selectedStage");
-		string level = stageID.Substring(0,2);
+		level = stageID.Substring(0,2);
 		int stageNum = int.Parse(stageID.Substring(3,2));
 
-		int templateNum = Random.Range (1, 3);
+		int templateNum = Random.Range (1, 2); //number of template +1
 		string templateName = level + "Template" + templateNum;
 		Debug.Log (templateName);
 
@@ -20,14 +35,86 @@ public class Global_Variable : MonoBehaviour {
 		GameObject.Find ("tsTemplate2").SetActive (false);
 		selectedTemplate.SetActive (true);
 
-
 		foreach (GameObject stageSprite in GameObject.FindGameObjectsWithTag("stageImage")) {
 			stageSprite.GetComponent<Image> ().sprite = stageImages [stageNum];
 		}
-		/*if (Equals (level, "ts")) {
-			GameObject.Find ("Image").GetComponent<Image> ().sprite = ;
-		}*/
 
+		RectTransform tempTargetPos;
+		int swapIndex;
+		if (Equals (level, "ts")) {
+			
+			pieceArrived = new bool[4];
+			for (int i = 0; i < 4; i++) {
+				pieceArrived [i] = false;
+			}
+
+			pieceObject = new GameObject[4];
+			piecePos = new RectTransform[4];
+			pieceObject [0] = GameObject.Find ("piece1");
+			pieceObject [1] = GameObject.Find ("piece2");
+			pieceObject [2] = GameObject.Find ("piece3");
+			pieceObject [3] = GameObject.Find ("piece4");
+			for (int i = 0; i < 4; i++) {
+				piecePos[i] = pieceObject[i].GetComponent<RectTransform> ();
+			}
+
+			pieceTargetPos = new RectTransform[4];
+			pieceTargetPos [0] = GameObject.Find ("piecePos1").GetComponent<RectTransform> ();
+			pieceTargetPos [1] = GameObject.Find ("piecePos2").GetComponent<RectTransform> ();
+			pieceTargetPos [2] = GameObject.Find ("piecePos3").GetComponent<RectTransform> ();
+			pieceTargetPos [3] = GameObject.Find ("piecePos4").GetComponent<RectTransform> ();
+			for (int i = 0; i < 4; i++) {
+				swapIndex = Random.Range (0, 4);
+				tempTargetPos = pieceTargetPos [swapIndex];
+				pieceTargetPos [swapIndex] = pieceTargetPos [i];
+				pieceTargetPos [i] = tempTargetPos;
+			}
+				
+		}
+
+
+
+
+		StartCoroutine (scrambleDelay ());
+
+	}
+
+	void Update(){
+		if (waiting == false) {
+			if (Equals (level, "ts")) {
+				pieceCount = 4;
+
+			}
+			for (int i = 0; i < pieceCount; i++) {
+				
+				if (Vector2.Distance (piecePos [i].position, pieceTargetPos [i].position) == 0 && pieceArrived [i] == false) {
+					pieceArrived [i] = true;
+					countArrived++;
+				} else if(pieceObject[i].GetComponent<Piece_Properties>().getDrag() == false) {
+					piecePos [i].position = Vector2.MoveTowards (piecePos [i].position, pieceTargetPos [i].position, pieceSpeed * Time.deltaTime);
+				}
+			}
+
+			if (countArrived>=pieceCount && reachTarget == false) {
+				reachTarget = true;
+
+
+				foreach (GameObject piece in GameObject.FindGameObjectsWithTag("piece")) {
+					piece.GetComponent<Piece_Properties>().toSmall();
+				}
+
+			}
+
+		}
+	}
+
+	IEnumerator scrambleDelay(){
+		yield return new WaitForSeconds(waitingTime);
+		waiting = false;
+	}
+
+	public string getLevel(){
+		return level;
 	}
 
 }
---
+++ Script/Piece_Properties.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece_Properties : MonoBehaviour {
+	public enum resizePatternX {RIGHT, LEFT, CENTER};
+	public enum resizePatternY {UP, DOWN, CENTER};
+	public resizePatternX xPositionPattern;
+	public resizePatternY yPositionPattern;
+
+	private RectTransform image;
+	public Vector2 smallImagePosition;
+	public Vector2 smallImageDimension;
+	public Vector2 smallDimension;
+	public Vector2 bigImagePosition;
+	public Vector2 bigImageDimension;
+	public Vector2 bigDimension;
+	private Vector2 scale;
+
+	public bool isDrag;
+
+	void Start () {
+		image = this.transform.Find ("Image").GetComponent<RectTransform> ();
+		string level = GameObject.Find ("Canvas").GetComponent<Global_Variable> ().getLevel ();
+		if (Equals (level, "ts")) {
+			scale = new Vector2(0.5f ,0.5f);
+		}
+		Debug.Log (scale);
+		bigDimension = this.GetComponent<RectTransform> ().sizeDelta;
+		smallDimension = Vector2.Scale(bigDimension,scale);
+		bigImageDimension = image.sizeDelta;
+		smallImageDimension = Vector2.Scale(bigImageDimension,scale);
+		bigImagePosition = image.localPosition;
+
+		float smallImagePositionX;
+		float smallImagePositionY;
+
+		switch (xPositionPattern) {
+		case resizePatternX.RIGHT:
+			smallImagePositionX = -((smallImageDimension.x - smallDimension.x) / 2); 
+			break;
+		case resizePatternX.LEFT:
+			smallImagePositionX = ((smallImageDimension.x-smallDimension.x) / 2);
+			break;
+		default:
+			smallImagePositionX = 0;
+			break;
+		}
+
+		switch (yPositionPattern) {
+		case resizePatternY.UP:
+			smallImagePositionY = -((smallImageDimension.y - smallDimension.y) / 2); 
+			break;
+		case resizePatternY.DOWN:
+			smallImagePositionY = ((smallImageDimension.y-smallDimension.y) / 2);
+			break;
+		default:
+			smallImagePositionY = 0;
+			break;
+		}
+
+		smallImagePosition = new Vector2 (smallImagePositionX, smallImagePositionY);
+	}
+
+	void Update(){
+	}
+
+	public void toSmall(){
+		this.GetComponent<RectTransform> ().sizeDelta = smallDimension;
+		image.sizeDelta = smallImageDimension;
+		image.localPosition = smallImagePosition;
+	}
+
+
+	public void drag(){
+		this.GetComponent<RectTransform> ().position = Input.mousePosition;
+		this.GetComponent<RectTransform> ().sizeDelta = bigDimension;
+		image.sizeDelta = bigImageDimension;
+		image.localPosition = bigImagePosition;
+		isDrag = true;
+	}
+
+	public void drop(){
+		toSmall ();
+		isDrag = false;
+	}
+
+	public bool getDrag(){
+		return isDrag;
+	}
+}
